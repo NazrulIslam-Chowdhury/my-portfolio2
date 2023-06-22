dcc09687485a22c4ded5083f57b55464250cd0fb
unknown <nazrulislam66188@gmail.com>
2023-06-22 11:09:08 +0600

    tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,7 +1,17 @@
+import { technologies } from '../constants';
+import Ball from './canvas/Ball';
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {
+        technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <Ball icon={technology.icon} />
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
