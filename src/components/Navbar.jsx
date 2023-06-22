196fda1c5eb4011fcca0e6c71871a45b8e8dafdc
unknown <nazrulislam66188@gmail.com>
2023-06-22 12:05:05 +0600

    work section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-24 h-24 object-contain" />
+          <img src={logo} alt="logo" className="w-32 h-32 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
             Nazrul <br /><span>Islam</span> Chowdhury
           </p>
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,15 @@
 import { technologies } from '../constants';
+import { staggerContainer } from '../utils/motion';
 import Ball from './canvas/Ball';
+import { motion } from 'framer-motion';
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <motion.section variants={staggerContainer()}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, amount: 0.25 }}
+      className="sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0 flex flex-row flex-wrap justify-center gap-10" >
       {
         technologies.map((technology) => (
           <div className='w-28 h-28' key={technology.name}>
@@ -11,7 +17,7 @@ const Tech = () => {
           </div>
         ))
       }
-    </div>
+    </motion.section>
   )
 }
 
