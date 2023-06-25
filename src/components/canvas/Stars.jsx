338dca0d8b26e55ef60fb46a01b2ea3dfc458468
unknown <nazrulislam66188@gmail.com>
2023-06-25 09:45:33 +0600

    stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -8,7 +8,7 @@ import { Suspense, useRef } from 'react';
 const Stars = (props) => {
   const ref = useRef();
 
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  const sphere = random.inSphere(new Float32Array(0), { radius: 1.2 });
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
